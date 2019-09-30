9f7df1b43497e11c3e759e1999a44a0f2d6fe3f3
wgrimshaw <walker.grimshaw@duke.edu>
2019-09-30 12:10:54 -0400

    Added front matter

---
+++ ARGOSTracker.py
@@ -0,0 +1,8 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Parses a line of ARGOS tracking data 
+#
+# Created by: Walker Grimshaw (walker.grimshaw@duke.edu)
+# Created on: Fall 2019
+#--------------------------------------------------------------
